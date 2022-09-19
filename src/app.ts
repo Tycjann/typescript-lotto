493eec8e6267775c4f5522c1ff7b0f5767bca5fc
Mirosław Gucwa <mgucwa@gmail.com>
2022-09-20 01:56:02 +0200

    End: App

--- src/app.ts
+++ src/app.ts
@@ -1,16 +1,27 @@
 import inquirer from 'inquirer';
 
-const chosenNumbers: Array<number> = [10];
+const chosenNumbers: Array<number> = [];
 const randomNumbers: Array<number> = [];
+const hitNumbers: Array<number> = [];
 
 const validateInput = (text: string): boolean => {
-
   const number = parseInt(text);
 
   if (!chosenNumbers.includes(number) && number >= 1 && number <= 49)
     return true;
   else
     return false;
+};
+
+const validateRandomNumber = (number: number): boolean => {
+  if (!randomNumbers.includes(number) && number >= 1 && number <= 49)
+    return true;
+  else
+    return false;
+};
+
+const randomNewNumber = (max: number): number => {
+  return Math.floor(Math.random() * max);
 };
 
 const startApp = async (): Promise<void> => {
@@ -23,13 +34,26 @@ const startApp = async (): Promise<void> => {
 
     if (validateInput(result.number)) {
       chosenNumbers.push(parseInt(result.number));
-      console.log('OK');
     }
   } while (chosenNumbers.length < 6);
+
+  do {
+    const randomNumber = randomNewNumber(49);
+
+    if (validateRandomNumber(randomNumber)) {
+      randomNumbers.push(randomNumber);
+    }
+  } while (randomNumbers.length < 6);
+
+  chosenNumbers.forEach(element => {
+    if (randomNumbers.includes(element)) hitNumbers.push(element);
+  });
+
+
   console.log('chosenNumbers:', chosenNumbers);
+  console.log('randomNumbers:', randomNumbers);
+  console.log('hitNumbers:', hitNumbers);
+  console.log('number of hit:', hitNumbers.length);
 };
-
-// validateInput('3');
-
 
 startApp();
